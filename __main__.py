c5de67976335514ed5f4a0a8a7856428b27b77cb
Qunfei Wu <wu.qunfei@gmail.com>
2022-02-10 20:50:02 +0100

    add configuration

--- __main__.py
+++ __main__.py
@@ -1,12 +1,7 @@
-from typing import Union
-
 from pydantic import validator
 from pydantic.dataclasses import dataclass
-
-
 import hydra
 from hydra.core.config_store import ConfigStore
-
 from src.containers import MyContainer
 
 
@@ -18,36 +13,27 @@ class MySQLConfig:
     password: str
 
     @validator('port', pre=True)
-    def check_port(cls, v):
-        if v > 3306:
-            raise Exception(f"Port is wrong {v}")
-        return v
-    #
-    # @validator('password', pre=True)
-    # def check_pwd(cls, v):
-    #     if not v or len(v) < 6:
-    #         raise Exception('f"pwd is too short"')
+    def check_port(cls, port):
+        if port < 1024:
+            raise Exception(f"Port:{port} < 1024 is forbidden ")
+        return port
 
 
 cs = ConfigStore.instance()
-cs.store(name="config", node=MySQLConfig, group='db')
+cs.store(name="config", node=MySQLConfig, group='')
 
 
 @hydra.main(config_path="", config_name="config")
 def my_app(cfg: MySQLConfig) -> None:
     """1. to get config yaml by hydra"""
     cfg_dict = dict(cfg)
-
     """2. to validate the configuration by pydantic"""
-    configuration = MySQLConfig(**cfg_dict)
-
+    MySQLConfig(**cfg_dict)
     container = MyContainer()
     """3. to load configuration into container"""
     container.config.from_dict(cfg_dict)
-    agcs_nlp = container.nlp_service_factory()
-    agcs_nlp.run_nlp()
-
-
+    nlp = container.nlp_service_factory()
+    nlp.run_nlp()
 
 
 if __name__ == "__main__":
